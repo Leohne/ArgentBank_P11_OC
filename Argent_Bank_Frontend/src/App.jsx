f1ab846dbaafa11b621d96847a539929d9f1bd92
Leohne <bricemagneville.pro@gmail.com>
2024-01-17 13:07:22 +0100

    First push

---
+++ Argent_Bank_Frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./component/Header"
+import Homepage from "./pages/Homepage"
+import SignIn from "./pages/SignIn"
+import User from "./pages/User"
+import Footer from "./component/Footer"
+
+function App() {
+  
+    return (
+        <BrowserRouter>
+            <>
+                <Header/>
+                <Routes>
+                    <Route index element={<Homepage/>}/>
+                    <Route path="/sign-in" element={<SignIn/>}/>
+                    <Route path="/user" element={<User/>}/>
+                </Routes>
+                <Footer/>
+            </>
+        </BrowserRouter>
+  )
+}
+
+export default App
